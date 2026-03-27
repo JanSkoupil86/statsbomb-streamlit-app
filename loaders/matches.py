a3e890e817febe27ab8b7518716db9011e56ea68
Jan Skoupil <111843508+JanSkoupil86@users.noreply.github.com>
2026-03-27 15:37:40 -0230

    Update matches.py

--- loaders/matches.py
+++ loaders/matches.py
@@ -1,1 +1,8 @@
+import pandas as pd
+from pathlib import Path
 
+DATA_PATH = Path(r"C:\Users\Scoby\Downloads\open-data-master\data")
+
+def load_matches(comp_id, season_id):
+    path = DATA_PATH / "matches" / str(comp_id) / f"{season_id}.json"
+    return pd.read_json(path)

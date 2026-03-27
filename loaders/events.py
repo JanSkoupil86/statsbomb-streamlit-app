100228211e4c2d85eeb099f18ff41d087318df1e
Jan Skoupil <111843508+JanSkoupil86@users.noreply.github.com>
2026-03-27 15:37:27 -0230

    Add file existence check in load_events function

--- loaders/events.py
+++ loaders/events.py
@@ -7,6 +7,9 @@ DATA_PATH = Path(r"C:\Users\Scoby\Downloads\open-data-master\data")
 def load_events(match_id):
     file_path = DATA_PATH / "events" / f"{match_id}.json"
 
+    if not file_path.exists():
+        raise FileNotFoundError(f"{file_path} not found")
+
     with open(file_path, encoding="utf-8") as f:
         data = json.load(f)
 

6ca4905cfe41f486d603d2aec1f911ead1fb9a5d
Jan Skoupil <111843508+JanSkoupil86@users.noreply.github.com>
2026-03-27 15:41:50 -0230

    Update matches.py

--- loaders/matches.py
+++ loaders/matches.py
@@ -1,8 +1,5 @@
 import pandas as pd
-from pathlib import Path
-
-DATA_PATH = Path(r"C:\Users\Scoby\Downloads\open-data-master\data")
 
 def load_matches(comp_id, season_id):
-    path = DATA_PATH / "matches" / str(comp_id) / f"{season_id}.json"
-    return pd.read_json(path)
+    url = f"https://raw.githubusercontent.com/statsbomb/open-data/master/data/matches/{comp_id}/{season_id}.json"
+    return pd.read_json(url)

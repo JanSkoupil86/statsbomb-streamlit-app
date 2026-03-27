c3e1e2cef5fbde2f3cac146ca424857717d8bc2b
Jan Skoupil <111843508+JanSkoupil86@users.noreply.github.com>
2026-03-27 15:41:29 -0230

    Update competitions.py

--- loaders/competitions.py
+++ loaders/competitions.py
@@ -1,7 +1,5 @@
 import pandas as pd
-from pathlib import Path
-
-DATA_PATH = Path(r"C:\Users\Scoby\Downloads\open-data-master\data")
 
 def load_competitions():
-    return pd.read_json(DATA_PATH / "competitions.json")
+    url = "https://raw.githubusercontent.com/statsbomb/open-data/master/data/competitions.json"
+    return pd.read_json(url)

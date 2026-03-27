6cca1901c52a11e4a114d9619a3b4ab1ac525673
Jan Skoupil <111843508+JanSkoupil86@users.noreply.github.com>
2026-03-27 15:13:45 -0230

    Update pitch.py

--- visuals/pitch.py
+++ visuals/pitch.py
@@ -1,1 +1,21 @@
+from mplsoccer import Pitch
+import matplotlib.pyplot as plt
 
+def shot_map(events, team):
+    pitch = Pitch()
+    fig, ax = pitch.draw()
+
+    shots = events[
+        (events["type.name"] == "Shot") &
+        (events["team.name"] == team)
+    ]
+
+    # Drop null locations
+    shots = shots[shots["location"].notnull()]
+
+    x = shots["location"].apply(lambda loc: loc[0])
+    y = shots["location"].apply(lambda loc: loc[1])
+
+    pitch.scatter(x, y, ax=ax)
+
+    return fig

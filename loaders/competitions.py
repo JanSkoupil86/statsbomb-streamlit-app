72a12896d774321020f2c4eed84cd6bc463761bc
Jan Skoupil <111843508+JanSkoupil86@users.noreply.github.com>
2026-03-27 15:38:02 -0230

    Update competitions.py

--- loaders/competitions.py
+++ loaders/competitions.py
@@ -1,1 +1,7 @@
+import pandas as pd
+from pathlib import Path
 
+DATA_PATH = Path(r"C:\Users\Scoby\Downloads\open-data-master\data")
+
+def load_competitions():
+    return pd.read_json(DATA_PATH / "competitions.json")
